f37c43bfbf65a59e0384a482d2c55748e39db296
Clement Olivier <oliclems@gmail.com>
2023-03-27 19:10:33 +0200

    feat(tp): tp01 complete

---
+++ src/test/java/fr/diginamic/enumerations/SaisonTest.java
@@ -0,0 +1,22 @@
+package fr.diginamic.enumerations;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SaisonTest {
+
+    @Test
+    public void testValueOfLibelle() throws InterruptedException {
+        String libelle = "Printemps";
+        String libelle2 = "Automne";
+        String libelle3 = "PasDeSaison";
+
+        assertEquals(Saison.PRINTEMPS, Saison.valueOfLibelle(libelle));
+        assertEquals(Saison.AUTOMNE, Saison.valueOfLibelle(libelle2));
+        assertEquals(null, Saison.valueOfLibelle(libelle3));
+    }
+}
